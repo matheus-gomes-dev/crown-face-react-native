b984ff40086b06ddd5d03f7bd9d01aaa36ab31aa
Matheus <matheus.lima@hypnobox.com.br>
2018-10-11 17:19:26 -0300

    components for other pages

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class About extends Component {
+  render() {
+    return (
+      <Text style={{ flex: 1, backgroundColor: '61BD8C' }}>
+        Aqui podem ser apresentadas informações sobre o jogo
+      </Text>
+    )
+  }
+}
